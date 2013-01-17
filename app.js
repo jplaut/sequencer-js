d52f2e3330a42712a27955660ba1066028f8c271
Jon Plaut <jp@tangerinesymphony.net>
2013-01-17 10:42:13 -0800

    starting

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-var express = require('express');
-var app = express();
+const express = require('express'),
+      http = require('http'),
+      https = require('https'),
+      fs = require('fs'),
+      app = express();
 
 app.use(express.static(__dirname));
 app.use(express.bodyParser());
-app.set('views', __dirname);
+app.set('views', __dirname + "/tpl");
+
+var privateKey = fs.readFileSync('privatekey.pem').toString();
+var certificate = fs.readFileSync('certificate.pem').toString();
+
+var credentials = {key: privateKey, cert: certificate};
 
 app.get('/', function(req, res) {
   var dev = (process.env.DEV) ? true: false;
-  res.render("home.ejs", {dev: dev});
+  res.render("home.ejs", {dev: dev, secureurl: process.env.SECUREURL});
 });
 
-var port = process.env.PORT || 4567;
+app.get('/login', function(req, res) {
+  if (req.secure) {
+    res.render("login.ejs");
+  } else {
+    res.send("Permission denied.");
+  }
+})
 
-app.listen(port, function() {
+var port = process.env.PORT || 4567;
+var securePort = process.env.SECUREPORT || 4568;
+
+http.createServer(app).listen(port, function() {
   console.log('Listening on port ' + port);
+});
+
+https.createServer(credentials, app).listen(securePort, function() {
+  console.log('Listening on port ' + securePort);
 });
