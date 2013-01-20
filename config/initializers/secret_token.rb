102ef90941c2c382b0d70b5195185410863f4e24
Jonathan Plaut and Thomas Bukowski <pair+jp+thomas@tangerinesymphony.net>
2013-01-19 16:49:53 -0800

    added rails, moved things into the right directory for railsland

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SequencerJs::Application.config.secret_token = '80d83d44eefab0564c7c5010309cc3dec8572581a8fcbcb74e44967b6ddfdef1d63b26dc0662b9c5efc7d9e6e828d12da4947161c3f3df4d7a39765f9f8aebb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SequencerJs::Application.config.session_store :cookie_store, key: '_sequencer-js_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SequencerJs::Application.config.session_store :active_record_store
